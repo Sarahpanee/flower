b9687321b3febbec50757c1d42b4e39e8e042d7b
panyl <yali.pan@hotmail.com>
2018-07-23 16:17:10 +0800

    Initial Commit

---
+++ flowerTests/flowerTests.swift
@@ -0,0 +1,36 @@
+//
+//  flowerTests.swift
+//  flowerTests
+//
+//  Created by panyl on 18/7/23.
+//  Copyright © 2018年 panyl. All rights reserved.
+//
+
+import XCTest
+@testable import flower
+
+class flowerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
