ab051cdce573e8f750aafcbbbb8bde3df4cd74ea
panyl <yali.pan@hotmail.com>
2018-07-24 09:08:03 +0800

    something to do

--- flowerTests/flowerTests.swift
+++ flowerTests/flowerTests.swift
@@ -14,6 +14,7 @@ class flowerTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        // something to do
     }
     
     override func tearDown() {
